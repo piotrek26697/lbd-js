7b4f5c575bd58e898ce7774cc8bc0bebd70793c5
piotr.musiol <piotrek26697@gmail.com>
2019-07-30 15:47:34 +0200

    Implement calculator without repeating previous function.

--- DOM_and_jQuery/01_calculator_task/calc.js
+++ DOM_and_jQuery/01_calculator_task/calc.js
@@ -1,3 +1,70 @@
 /*
  * Implement all your JavaScript in this file!
+ *  $('#output').html('test');
  */
+var number1 = null;
+var number2 = null;
+var operation = null;
+var clearPermission = true;
+
+$("button[id^='button']").click(showNumber);
+$("button[id$='Button']").click(setOperation);
+
+function cleanDisplay() {
+    if (clearPermission) {
+        $('#display').val("");
+        clearPermission = false;
+    }
+}
+
+function showNumber() {
+    cleanDisplay();
+    $('#display').val($('#display').val() + ($(this).val()));
+}
+
+function setOperation() {
+    cleanDisplay();
+    clearPermission = true;
+
+    if (number1 === null)
+        number1 = $('#display').val();
+    else
+        number2 = $('#display').val();
+
+    switch (this.id) {
+        case "addButton":
+            doMath();
+            operation = "+";
+            break;
+        case "subtractButton":
+            doMath();
+            operation = "-";
+            break;
+        case "multiplyButton":
+            doMath();
+            operation = "*";
+            break;
+        case "divideButton":
+            doMath();
+            operation = "/";
+            break;
+        case "clearButton":
+            number1 = null;
+            number2 = null;
+            operation = null;
+            $('#display').val("");
+            break;
+        case "equalsButton":
+            doMath();
+
+    }
+    function doMath() {
+        if (number1 && number2) {
+            var result = eval(number1 + operation + number2);
+            $('#display').val(result);
+            number1 = result;
+            number2 = null;
+            operation = null;
+        }
+    }
+}
